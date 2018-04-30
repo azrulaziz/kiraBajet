744a6a07772f9f8b1adc7dfba815cea3b15b9641
Azrul Aziz <azrultres@gmail.com>
2018-05-01 03:00:49 +0800

    v0.1

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/playground/redux-101.js',
+    entry: './src/app.js',
     output: {
         path: path.join(__dirname, 'public'),
         filename: 'bundle.js'
@@ -29,4 +29,4 @@ module.exports = {
     }
 };
 
-// loader 
+
