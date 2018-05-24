91bacee61ad424e32b2dde801dcb8448bc3bb3f4
Azrul Aziz <azrultres@gmail.com>
2018-05-25 00:38:13 +0800

    v0.2 react-dates

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import moment from 'moment';
+import { SingleDatePicker } from 'react-dates';
+import 'react-dates/initialize';
+import 'react-dates/lib/css/_datepicker.css';
+
 
 export default class ExpenseForm extends React.Component {
     state = {
         description: '',
-        notes: ''
+        notes: '',
+        amount: '',
+        createdAt: moment(),
+        focused: false,
     }
 
     handleDescriptionChange = (e) => {
@@ -14,6 +22,23 @@ export default class ExpenseForm extends React.Component {
     handleNotesChange = (e) => {
         const notes = e.target.value;
         this.setState({notes})
+    }
+
+    handleAmountChange = (e) => {
+        const amount = e.target.value;
+        const regex = /^\d*(\.\d{0,2})?$/;
+
+        if (amount.match(regex)) {
+            this.setState({amount})
+        }
+    }
+
+    handleDateChange = (createdAt) => {
+       this.setState({createdAt});
+    }
+
+    handleFocusChange = ({focused}) => {
+        this.setState({focused});
     }
 
     render() {
@@ -27,7 +52,20 @@ export default class ExpenseForm extends React.Component {
                         value={this.state.description}
                         onChange={this.handleDescriptionChange}
                     />
-                    <input type="number" placeholder="Amount" />
+                    <input 
+                        type="text" 
+                        placeholder="Amount" 
+                        value={this.state.amount}
+                        onChange={this.handleAmountChange}
+                    />
+                    <SingleDatePicker 
+                        date={this.state.createdAt}
+                        onDateChange={this.handleDateChange}
+                        focused={this.state.focused}
+                        onFocusChange={this.handleFocusChange}
+                        numberOfMonths={1}
+                        isOutsideRange={() => false}
+                    />
                     <textarea 
                         placeholder="Add Notes"
                         value={this.state.notes}
